4c244e2ba87e56051659787ae96e895dc2ac92b7
dipankar7645 <dipankar11092003@gmail.com>
2025-04-28 12:30:41 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,14 @@
+const path = require('path')
 require('dotenv').config()
+const express = require('express')
 const app = require('./src/app')
 
 const port = process.env.PORT || 3000;
-
+const _dirname =path.resolve()
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+})
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
 })
